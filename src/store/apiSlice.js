67a0592d2bf9e08315c49f36b701dbfe1cc35ae7
linarifux <linarifux@gmail.com>
2022-07-02 02:11:35 +0600

    Update Base URL address for all request

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const baseURI = 'http://localhost:8080'
+const baseURI = 'https://expense-tracker-pet-project.herokuapp.com/'
 
 export const apiSLice = createApi({
     baseQuery: fetchBaseQuery({baseUrl: baseURI}),
